547492aa52e67d9eae859d8ac7e0b1a5f2f9997c
L Turnbull <l-turnbull@outlook.com>
2023-04-28 22:17:17 +0100

    Completed GUI requirements.

--- cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/GameGUI.java
+++ cwk4-studentsIJ/cwk4-studentsIJ/src/cwk4/GameGUI.java
@@ -10,7 +10,7 @@ import java.util.*;
  * @author A.A.Marczyk
  * @version 20/10/23
  */
-public class GameGUI 
+public class GameGUI
 {
     private WIN gp = new SpaceWars("Horatio");
     private JFrame myFrame = new JFrame("Game GUI");
@@ -18,6 +18,8 @@ public class GameGUI
     private JTextArea listing = new JTextArea();
     private JLabel codeLabel = new JLabel ();
     private JButton fightBtn = new JButton("Fight");
+    private JButton viewStateBtn = new JButton("View Game State");
+    private JButton clearBtn = new JButton("Clear Screen");
     private JPanel eastPanel = new JPanel();
 
     
@@ -43,7 +45,26 @@ public class GameGUI
         listForcesItem.addActionListener(new ListForcesHandler());
         forcesMenu.add(listForcesItem);
 
+        JMenuItem listASFItem = new JMenuItem("List ASF");
+        listASFItem.addActionListener(new ListASFHandler());
+        forcesMenu.add(listASFItem);
 
+        JMenuItem activateForceItem = new JMenuItem("Activate Force");
+        activateForceItem.addActionListener(new ActivateForceHandler());
+        forcesMenu.add(activateForceItem);
+
+        JMenuItem recallForceItem = new JMenuItem("Recall Force");
+        recallForceItem.addActionListener(new RecallForceHandler());
+        forcesMenu.add(recallForceItem);
+
+
+        // Create the Battles menu
+        JMenu battlesMenu = new JMenu("Battles");
+        menubar.add(battlesMenu);
+
+        JMenuItem listBattlesItem = new JMenuItem("List Battles");
+        listBattlesItem.addActionListener(new ListBattlesHandler());
+        battlesMenu.add(listBattlesItem);
     }
     /**
      * Create the Swing frame and its content.
@@ -59,19 +80,37 @@ public class GameGUI
         eastPanel.add(fightBtn);
         fightBtn.addActionListener(new FightBtnHandler());
         fightBtn.setVisible(true);
+
+        eastPanel.add(viewStateBtn);
+        viewStateBtn.addActionListener(new ViewStateBtnHandler());
+        viewStateBtn.setVisible(true);
+
+        eastPanel.add(clearBtn);
+        clearBtn.addActionListener(new ClearBtnHandler());
+        clearBtn.setVisible(true);
         
         // building is done - arrange the components and show        
         myFrame.pack();
         myFrame.setVisible(true);
     }
-    
+
+    private String activation(int code)
+    {
+        return switch (code) {
+            case 0 -> "force is activated";
+            case 1 -> "force is not in the UFFDock";
+            case 2 -> "not enough money";
+            case -1 -> "no such force";
+            default -> "Error";
+        };
+    }
     
     private String fighting(int code)
     {
         switch (code)
         {
-            case 0:return "Fight won"; 
-            case 1:return "Fight lost as no suitable force available"; 
+            case 0:return "Fight won";
+            case 1:return "Fight lost as no suitable force available";
             case 2:return "Fight lost on battle strength, force destroyed";
             case 3:return "fight is lost and admiral completely defeated ";
         }
@@ -88,6 +127,38 @@ public class GameGUI
             
         }
     }
+
+    private class ListASFHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            listing.setVisible(true);
+            String asfDetails = gp.getASFleet();
+            listing.setText(asfDetails);
+        }
+    }
+
+    private class ActivateForceHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String input = JOptionPane.showInputDialog("Enter the force's fleet reference: ");
+            int result = gp.activateForce(input);
+            JOptionPane.showMessageDialog(myFrame, activation(result));
+        }
+    }
+
+    private class RecallForceHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String input = JOptionPane.showInputDialog("Enter the force's fleet reference: ");
+            JOptionPane.showMessageDialog(myFrame, "Recalling force...");
+            gp.recallForce(input);
+        }
+    }
+
+    private class ListBattlesHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            listing.setVisible(true);
+            String battlesDetails = gp.getAllBattles();
+            listing.setText(battlesDetails);
+        }
+    }
     
     
     
@@ -99,7 +170,21 @@ public class GameGUI
             String inputValue = JOptionPane.showInputDialog("Fight number ?: ");
             int num = Integer.parseInt(inputValue);
             result = gp.doBattle(num);
-            JOptionPane.showMessageDialog(myFrame,fighting(result));    
+            JOptionPane.showMessageDialog(myFrame,fighting(result));
+        }
+    }
+
+    private class ViewStateBtnHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            listing.setVisible(true);
+            String stateResults = gp.toString();
+            listing.setText(stateResults);
+        }
+    }
+
+    private class ClearBtnHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            listing.setVisible(false);
         }
     }
     
